28ba918450d3f7e62ce0c658dc3ce14a39aee7fc
David Paz <davidpaz@MacBook-Air-de-David.local>
2017-02-18 08:21:36 -0600

    se repo oficial de grails para los plugins

--- webserver/grails-app/conf/Config.groovy
+++ webserver/grails-app/conf/Config.groovy
@@ -85,8 +85,10 @@ environments {
         // TODO: grails.serverURL = "http://www.changeme.com"
         //domainMainUsers         = 'http://ec2-54-148-189-131.us-west-2.compute.amazonaws.com'
         //domainMainBands         = 'http://ec2-52-10-76-170.us-west-2.compute.amazonaws.com'
-        domainMainUsers         = 'http://api.maxibanda.com.mx'
-        domainMainBands         = 'http://api.maxibanda.com.mx'
+        //domainMainUsers         = 'http://api.maxibanda.com.mx'
+        //domainMainBands         = 'http://api.maxibanda.com.mx'
+        domainMainUsers = 'http://localhost:8080'
+        domainMainBands = 'http://localhost:8080'
     }
 }
 
